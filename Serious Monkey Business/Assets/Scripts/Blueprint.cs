1e0469f88a4b7a7bc0e15fc9ee2ef535d6e2e4df
Joey Marten <83598831+FrankfurtOceanic@users.noreply.github.com>
2022-06-16 19:56:34 -0400

    Changed get down

--- Serious Monkey Business/Assets/Scripts/Blueprint.cs
+++ Serious Monkey Business/Assets/Scripts/Blueprint.cs
@@ -11,6 +11,9 @@ public class Blueprint : MonoBehaviour
     [SerializeField] LayerMask targetLayer;
     [Range(0f, 1f)]
     public float TriggerThreshold = 0.1f;
+    [Range(0f, 1f)]
+    public float getDownThreshhold = 0.7f;
+    bool released;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,15 +32,21 @@ public class Blueprint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > TriggerThreshold)
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) < getDownThreshhold)
+        {
+            released = true;
+        }
+            if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > TriggerThreshold)
         {
             building.SetActive(true);
             if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 50000f, targetLayer))
             {
                 building.transform.position = hit.point;
-                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+
+                if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > getDownThreshhold && released)
                 {
                     Instantiate(turret, hit.point, Quaternion.identity);
+                    released = false;
                 }
             }
         }
