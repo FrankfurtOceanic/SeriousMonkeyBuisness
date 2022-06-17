bf05ad03bad867f7865921e35b56c693801b4095
Joey Marten <83598831+FrankfurtOceanic@users.noreply.github.com>
2022-06-16 20:47:10 -0400

    Changed to toggle mode

--- Serious Monkey Business/Assets/Scripts/Blueprint.cs
+++ Serious Monkey Business/Assets/Scripts/Blueprint.cs
@@ -13,7 +13,7 @@ public class Blueprint : MonoBehaviour
     public float TriggerThreshold = 0.1f;
     [Range(0f, 1f)]
     public float getDownThreshhold = 0.7f;
-    bool released;
+    bool Building = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,7 +32,7 @@ public class Blueprint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) < getDownThreshhold)
+        /*if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) < getDownThreshhold)
         {
             released = true;
         }
@@ -53,7 +53,31 @@ public class Blueprint : MonoBehaviour
         else
         {
             building.SetActive(false);
+        }*/
+
+        if (OVRInput.GetDown(OVRInput.RawButton.Y))
+        {
+            if (Building) Building = false;
+            else Building = true;
+
         }
-        
+        if (Building)
+        {
+            building.SetActive(true);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 50000f, targetLayer))
+            {
+                building.transform.position = hit.point;
+
+                if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
+                {
+                    Instantiate(turret, hit.point, Quaternion.identity);
+                }
+            }
+        }
+        else
+        {
+            building.SetActive(false);
+        }
+
     }
 }
